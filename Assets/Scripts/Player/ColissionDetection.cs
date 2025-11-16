12e9833b8d5f3d57a09f9d50ffa2181fa2103e64
VBDP <vbd@vbd.es>
2025-11-16 11:29:32 +0100

    Ahora el jugador choca con las paredes de forma simulada y hacia alante no puede colarse entre las esquinas ni la puerta, pero hacia atrás si puede colarse.

---
+++ Assets/Scripts/Player/ColissionDetection.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class ColissionDetection : MonoBehaviour
+{
+    public float pushBackForce = 5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            Vector3 pushDir = (transform.position - other.ClosestPoint(transform.position)).normalized;
+            transform.position += pushDir * pushBackForce * Time.deltaTime;
+        }
+    }
+}
