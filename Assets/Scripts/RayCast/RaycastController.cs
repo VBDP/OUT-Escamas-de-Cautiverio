4497603d17e75a89d85b90b9479a8d1ff9abc9d6
VBDP <vbd@vbd.es>
2025-11-17 19:24:30 +0100

    Ahora si haces click en la puerta rota hacia fuera mientras mantienes y el raycast detecta puertas, falta el outline

--- Assets/Scripts/RayCast/RaycastController.cs
+++ Assets/Scripts/RayCast/RaycastController.cs
@@ -2,34 +2,20 @@ using UnityEngine;
 
 public class RaycastController : MonoBehaviour
 {
-    public float rayDistance  = 10f;
-    public LineRenderer lineRenderer;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public bool hittingDoor = false;
+    public void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-         // Crear un rayo desde la posición de la cámara hacia adelante
-        Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
-
-        // Ejecutar el raycast
-        if (Physics.Raycast(ray, out hit, rayDistance))
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 5))
         {
-            Debug.Log("Impactó con: " + hit.collider.name);
-            
-            // Opcional: dibujar el rayo en la escena para depuración
-            Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.red);
-        }
-        else
-        {
-            // Raycast no impactó, dibujar rayo máximo
-            Debug.DrawRay(transform.position, transform.forward * rayDistance, Color.green);
+            if (hit.collider.gameObject.tag == "Door")
+            {
+                if (Input.GetMouseButton(0))
+                {                
+                        hit.transform.Rotate(Vector3.down);                   
+                }
+            }
         }
     }
 }
+
