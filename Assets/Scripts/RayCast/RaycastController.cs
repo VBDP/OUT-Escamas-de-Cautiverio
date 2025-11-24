aa6ea89024fbfa19b05082cc03ec3109320ae491
VBDP <vbd@vbd.es>
2025-11-24 10:42:55 +0100

    Ahora el spawn de la llave es aleatorio pero ha perdido el sistema de outline al dar errores

--- Assets/Scripts/RayCast/RaycastController.cs
+++ Assets/Scripts/RayCast/RaycastController.cs
@@ -6,17 +6,17 @@ public class RaycastController : MonoBehaviour
     public bool hittingDoor = false;
     public Inventory inventory;
     public Outline outline;
-    public Outline outlineKey;
     public Outline outlineDoubleDoor;
     public Outline NPCOutline;
     public TextMeshProUGUI interactionText;
+
+    
     public void Update()
     {
         interactionText.text = "";
         bool keyFirstDoor = inventory.returnKeyFirstDoor(this);
                     RaycastHit hit;
         outline.OutlineColor = new Color(0f, 0f, 0f, 0f); //No se ve el outline en la puerta
-        outlineKey.OutlineColor = new Color(1f, 1f, 1f, 0f);
         outlineDoubleDoor.OutlineColor = new Color(1f, 1f, 1f, 0f);
         NPCOutline.OutlineColor = new Color(0f,0f,0f,0f);
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 5))
@@ -49,7 +49,6 @@ public class RaycastController : MonoBehaviour
             if(hit.collider.tag == "Collectable")
             {
                 interactionText.text = "Click to grab";
-                outlineKey.OutlineColor = new Color(1f, 1f, 1f,1f); //La llave se vuelve blanca al hacer hover
                 if (Input.GetMouseButton(0))
                 {
                     inventory.GetKeyFirstDoor();
@@ -57,11 +56,10 @@ public class RaycastController : MonoBehaviour
                     
                 }
             }
-
+            
             if (hit.collider.tag == "Trigger")
             {
                 interactionText.text = "Click to interact";
-                outlineKey.OutlineColor = new Color(1f, 1f, 1f, 1f); //La llave se vuelve blanca al hacer hover
                 outlineDoubleDoor.OutlineColor = new Color(1f, 1f, 1f, 1f);
                 if (Input.GetMouseButtonDown(0))
                 {
@@ -75,7 +73,7 @@ public class RaycastController : MonoBehaviour
 
             if(hit.collider.tag == "NPC" && Input.GetMouseButton(0))
             {
-                interactionText.text = "Assassin: ¿Puedes ver la palanca al lado de la puerta doble de madera?, Tira de ella y hayarás lo que buscas";
+                interactionText.text = "Assassin: ï¿½Puedes ver la palanca al lado de la puerta doble de madera?, Tira de ella y hayarï¿½s lo que buscas";
                 
             }
             else if (hit.collider.tag == "NPC" && !Input.GetMouseButton(0)){
