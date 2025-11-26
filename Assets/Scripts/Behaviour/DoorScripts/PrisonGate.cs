938adbc1e50ed628366b4bf97416877609b0c83c
VBDP <vbd@vbd.es>
2025-11-26 13:24:12 +0100

    cambiado idle del orco a carpeta de animaciones y nombre para saber que es

--- Assets/Scripts/Behaviour/DoorScripts/PrisonGate.cs
+++ Assets/Scripts/Behaviour/DoorScripts/PrisonGate.cs
@@ -4,38 +4,62 @@ public class PrisonGate : MonoBehaviour,DoorInterface
 {
     DoorInterface doorInterface;
     bool isOpen = false;
+    public RaycastController raycastController;
+    public bool haveKey = false;
+    public Inventory inventory;
 
     public void Start()
     {
         doorInterface = this as DoorInterface;
+        raycastController = GameObject.Find("First Person Camera").GetComponent<RaycastController>();
+        inventory = GameObject.Find("Player").GetComponent<Inventory>();
+        if(raycastController)
+        {
+            Debug.LogError("RaycastController found on First Person Camera");
+        }
+        if(inventory)
+        {
+            Debug.LogError("Inventory found on Player");
+        }
     }
 
 public void Update()
     {
-        isOpened();
+       
+                isOpened();
         doorInterface.OpenCloseDoor();
     }
 
     void DoorInterface.OpenCloseDoor()
     {
-       if(!isOpen && transform.localRotation.y > -0.1f)
-        {   
-                transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y - 0.1f, transform.localRotation.eulerAngles.z);  
-        }
-        else
+        if(raycastController.GetHitObjectName() == "door01")
         {
-            transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y + 0.1f, transform.localRotation.eulerAngles.z);
+            Debug.Log("Prison Gate interacted");
+            if(Input.GetMouseButton(0))
+            {
+                if(isOpen == false)
+                {
+                    transform.Rotate(this.transform.localRotation.x,-1f,this.transform.localRotation.z);
+                }
+                else if(isOpen == true)
+                {
+                    transform.Rotate(this.transform.localRotation.x,1f,this.transform.localRotation.z);
+                }
+            }
         }
     }
 
     void isOpened()
     {
-        if(transform.localRotation.y <= -0.1f)
+        if(transform.localRotation.y <= -0.7f && haveKey)
         {
             isOpen = true;
         }
-        else if(transform.localRotation.y >= 0f)
+        else if(transform.localRotation.y >= 0f && !haveKey)
         {
+            isOpen = false;
+        }
+        else{
             isOpen = false;
         }
 
