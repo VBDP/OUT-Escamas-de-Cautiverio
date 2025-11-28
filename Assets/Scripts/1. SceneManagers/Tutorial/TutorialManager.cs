61facdfc398ee7e4e47797c0b449429b6de35e10
VBDP <vbd@vbd.es>
2025-11-28 11:36:31 +0100

    Outline al hacer hover en la puerta, si estás delante se abre y detrás se cierra, Si no tienes llave te avisa en el texto del hud

---
+++ Assets/Scripts/1. SceneManagers/Tutorial/TutorialManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        
+    }
+
+
+}
+ 
--- Assets/Scripts/Behaviour/DoorScripts/PrisonGate.cs
+++ Assets/Scripts/Behaviour/DoorScripts/PrisonGate.cs
@@ -1,59 +1,30 @@
-using UnityEngine;
-using UnityEngine.UI;
+using System;
+using TMPro;
+using UnityEngine; 
 public class PrisonGate : MonoBehaviour,DoorInterface
 {
+    [SerializeField] private RaycastController Raycast; //Adquirimos el Raycast desde la escena.
+    [SerializeField] private Transform player;
+    [SerializeField] private TextMeshProUGUI InteractionText;
+    private string objectName;
+    private string objectTag;
+    private Outline objectOutline;
+    private Animator animator;
+    private float DoorRotation = 0f;
+    private float dot;
 
-    DoorInterface doorInterface;
-    private RaycastController raycastController;
     private bool haveKey = false;
-    private bool isOpen = false;
-    private Outline outline;
-    public TMPro.TextMeshProUGUI InteractionText;
-
-//----------------------------------------------------------------------------------------------------------------------
-    public void Start()
-    {       
-        outline = GetComponent<Outline>();
-        doorInterface = this as DoorInterface;
-        raycastController = GameObject.Find("First Person Camera").GetComponent<RaycastController>();
-    }
-
-public void Update()
+        public void Start() { objectOutline = GetComponent<Outline>(); animator = GetComponent<Animator>();}
+    public void Update()
     {
-        doorInterface.OpenCloseDoor();
-        outlinePuerta();
-        comprobarApertura();
+        OutlineChanger(); //Cambia el color del outline.
+        CalculatePlayerPosition(); //Calculamos si está delante o detrás de la puerta.
+        ChangeInteractionText();
+        ((DoorInterface)this).OpenCloseDoor(); //Si el usuario está delante de la puerta, la abre, si está detrás la cierra
     }
 
-//----------------------------------------------------------------------------------------------------------------------
-    void DoorInterface.OpenCloseDoor()
-    {
-       
-    }
-
-    void outlinePuerta()
-    {
-         if(raycastController.GetHitObjectName() == "Prison Gate")
-        {
-           outline.OutlineColor = Color.white;
-        }
-        else
-        {
-            outline.OutlineColor = new Color(0,0,0,0);
-        }
-    }
-
-    void comprobarApertura()
-    {
-        if(transform.localRotation.y <= -0.7f || transform.localRotation.y >= 0.7f)
-        {
-            isOpen = true;
-        }
-        else
-        {
-            isOpen = false;
-        }
-    }
-
-//----------------------------------------------------------------------------------------------------------------------
+    void OutlineChanger() { if (Raycast.GetHitObjectName() == "Prison Gate") { objectOutline.OutlineColor = Color.white; objectOutline.OutlineWidth = 2.0f; } else { objectOutline.OutlineColor = new Color(0, 0, 0, 0); } }
+    public void CalculatePlayerPosition() { Vector3 dir = (player.position - transform.position).normalized; dot = Vector3.Dot(transform.right, dir); Debug.Log(dot); }
+    void ChangeInteractionText() { if (Raycast.GetHitObjectName() == "Prison Gate") { if (!haveKey) { InteractionText.text = "Necesitas una llave para abrir esta puerta"; } } else { InteractionText.text = ""; } }
+    void DoorInterface.OpenCloseDoor() { if (Raycast.GetHitObjectName() == "Prison Gate" && Input.GetMouseButton(0)) { if (animator.GetFloat("DoorRotation") < 5 && dot >= 0) { DoorRotation += 1.5f * Time.deltaTime; animator.SetFloat("DoorRotation", DoorRotation); } else if (animator.GetFloat("DoorRotation") >= 0 && dot < 0) { DoorRotation -= 1.5f * Time.deltaTime; animator.SetFloat("DoorRotation", DoorRotation); } } }
 }
