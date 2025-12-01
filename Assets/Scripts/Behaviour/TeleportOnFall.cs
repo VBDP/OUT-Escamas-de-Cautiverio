747b5c2c0031b92b81853ff6a892bfca3a1e3bcf
VBDP <vbd@vbd.es>
2025-12-01 11:55:53 +0100

    Ahora el jugador respawnea cuando cae, bugs arreglados

--- Assets/Scripts/Behaviour/TeleportOnFall.cs
+++ Assets/Scripts/Behaviour/TeleportOnFall.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class TeleportOnFall : MonoBehaviour
 {
     public GameObject teleportTarget;
+    public GameObject player;   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            other.transform.position = teleportTarget.transform.position; // Teleport the player to the specified coordinates
-            other.transform.rotation = teleportTarget.transform.rotation; // Optional: Reset the player's rotation
+            player.transform.position = teleportTarget.transform.position; // Teleport the player to the specified coordinates
+            player.transform.rotation = teleportTarget.transform.rotation; // Optional: Reset the player's rotation
         }
     }
 }
