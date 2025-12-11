e2a2108b3cbf3bed63a6ae6591af76b72b8d9d01
VBDP <vbd@vbd.es>
2025-12-11 11:11:32 +0100

    Exit game y Play tutorial y New Game funcionando

--- Assets/Scripts/1. SceneManagers/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/1. SceneManagers/MainMenu/MainMenuManager.cs
@@ -5,10 +5,15 @@ using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour
 {
 
+
   public void OpenTutorial()
     {
         SceneManager.LoadSceneAsync("Tutorial");
+    }
 
+      public void OpenLevel1()
+    {
+        SceneManager.LoadSceneAsync("Level1");
     }
 
   public void ExitGame()
