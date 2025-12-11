aff0cfd0c5444e576f62fa866d6ea1a8e5cf3566
VBDP <vbd@vbd.es>
2025-12-11 11:33:24 +0100

    Ahora Options, Leaderboard y Credits abren un panel del main menu

--- Assets/Scripts/1. SceneManagers/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/1. SceneManagers/MainMenu/MainMenuManager.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
 
 
 public class MainMenuManager : MonoBehaviour
 {
 
-
+  [SerializeField] private List<GameObject> Panels;
   public void OpenTutorial()
     {
         SceneManager.LoadSceneAsync("Tutorial");
@@ -15,6 +17,45 @@ public class MainMenuManager : MonoBehaviour
     {
         SceneManager.LoadSceneAsync("Level1");
     }
+
+        public void OpenMenu()
+    {
+    foreach (GameObject panel in Panels)
+    {
+        panel.SetActive(false);
+    }
+      Panels[0].SetActive(true);
+    }
+
+    public void OpenCredits()
+    {
+    foreach (GameObject panel in Panels)
+    {
+        panel.SetActive(false);
+    }
+      Panels[1].SetActive(true);
+    }
+
+        public void OpenOptions()
+    {
+    foreach (GameObject panel in Panels)
+    {
+        panel.SetActive(false);
+    }
+      Panels[2].SetActive(true);
+    }
+
+        public void OpenLeaderboard()
+    {
+    foreach (GameObject panel in Panels)
+    {
+        panel.SetActive(false);
+    }
+      Panels[3].SetActive(true);
+    }
+    
+
+
 
   public void ExitGame()
 {
