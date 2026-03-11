eac55144fdb26d8748638145e9583ce5a4bd94c0
VBDP <vbd@vbd.es>
2026-03-11 11:47:16 +0100

    Cambiado script del submit que decide si haces rate o vas al main menu

--- Assets/Scripts/1. Managers/SceneSwitch.cs
+++ Assets/Scripts/1. Managers/SceneSwitch.cs
@@ -1,11 +1,43 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class SceneSwitch : MonoBehaviour
 {
     [SerializeField] private string selectedScene;
     [SerializeField] private GameObject ratingPanel;
+    [SerializeField] private bool hasRated;
+    [SerializeField] private TextMeshProUGUI submitText;
+
+    public void Start()
+    {
+            if (submitText != null)
+            {
+                if(hasRated)
+                {
+                    submitText.text = "Main Menu";
+                }
+                else
+                {
+                    submitText.text = "Rate the game";
+                }
+            }
+    }
+
+    public void selectAction()
+    {
+        if (!hasRated)
+        {
+            EnableRatingPanel();
+        }
+        else
+        {
+            SwitchScene();
+        }
+    }
+
     public void SwitchScene()
     {
         SceneManager.LoadSceneAsync(selectedScene);
